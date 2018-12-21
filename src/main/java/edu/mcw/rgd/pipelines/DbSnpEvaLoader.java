8b7dd5d4598d56e2ceb480f33a15dad6f27b2322
Marek Tutaj <mtutaj@mcw.edu>
2018-12-21 17:34:02 -0600

    Eva parser -- most code complete

--- src/main/java/edu/mcw/rgd/pipelines/DbSnpEvaLoader.java
+++ src/main/java/edu/mcw/rgd/pipelines/DbSnpEvaLoader.java
@@ -12,10 +12,13 @@ import edu.mcw.rgd.process.Utils;
 
 import java.io.*;
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Map;
 
 /**
  * Created by mtutaj on 12/13/2018.
+ * <p>
+ * TODO: parse studyIds and strains
  */
 public class DbSnpEvaLoader {
 
@@ -24,10 +27,14 @@ public class DbSnpEvaLoader {
 
     public void run(String dir, String source, int mapKey, String groupLabel, String dumpFileName) throws Exception {
 
+        String outFileName = dir + source + "_" +groupLabel+".json";
+
+        String fname = outFileName + ".gz";
+        processFile(fname);
+
         BufferedWriter dump = new BufferedWriter(new FileWriter(dumpFileName));
 
         // json file
-        String outFileName = dir + source + "_" +groupLabel+".json";
         BufferedWriter out = new BufferedWriter(new FileWriter(outFileName));
         JsonFactory jf = new JsonFactory();
         JsonGenerator jsonGenerator = jf.createGenerator(out);
@@ -128,36 +135,190 @@ public class DbSnpEvaLoader {
     // process json file with all variants
     void processFile( String fname ) throws Exception {
 
+        BufferedReader br = Utils.openReader(fname);
         JsonFactory jf = new JsonFactory();
-        JsonParser jp = jf.createParser(new BufferedReader(new FileReader(fname)));
-        if (jp.nextToken() != JsonToken.START_OBJECT) {
-            throw new IOException("Expected data to start with an Object");
-        }
+        JsonParser jp = jf.createParser(br);
 
-        // reference is the first token
-        while (jp.nextToken() != null) {
-            String fieldname = jp.getCurrentName();
-            /*
-            if ("variants".equals(fieldname)) {
-                jParser.nextToken();
-                parsedName = jParser.getText();
-            }
+        // keep track on the current depth of 'json objects':
+        // new DB_SNP objects starts at 'jsonObjectDepth == 2'
+        int jsonObjectDepth = 0;
 
-            if ("age".equals(fieldname)) {
-                jParser.nextToken();
-                parsedAge = jParser.getIntValue();
-            }
+        DbSnp dbSnp = null;
+        JsonToken token;
+        while( (token=jp.nextToken()) != null) {
+            if( token == JsonToken.START_OBJECT ) {
+                jsonObjectDepth++;
 
-            if ("address".equals(fieldname)) {
-                jParser.nextToken();
-                while (jParser.nextToken() != JsonToken.END_ARRAY) {
-                    addresses.add(jParser.getText());
+                if( jsonObjectDepth==2 ) {
+                    System.out.println("start new db_snp object");
+                    dbSnp = new DbSnp();
+                    dbSnp.setMapKey(360);
+                }
+            } else if( token == JsonToken.END_OBJECT ) {
+                jsonObjectDepth--;
+
+                if( jsonObjectDepth==1 ) {
+                    System.out.println("save db_snp data");
+                    save(dbSnp);
                 }
             }
-            */
+
+            String fieldName = jp.getCurrentName();
+            if( fieldName==null ) {
+                continue;
+            }
+            switch( fieldName ) {
+                case "alternate":
+                case "altAllele":
+                    String allele = jp.nextTextValue();
+                    if( dbSnp.getAllele()!=null ) {
+                        if( !allele.equals(dbSnp.getAllele()) ) {
+                            throw new Exception("allele override");
+                        }
+                    } else {
+                        dbSnp.setAllele(allele);
+                    }
+                    break;
+                case "mafAllele":
+                    String mafAllele = jp.nextTextValue();
+                    if( dbSnp.getMafAllele()!=null ) {
+                        if( !mafAllele.equals(dbSnp.getMafAllele()) ) {
+                            throw new Exception("maf allele override");
+                        }
+                    } else {
+                        dbSnp.setMafAllele(mafAllele);
+                    }
+                    break;
+                case "maf":
+                    jp.nextToken();
+                    dbSnp.setMafFrequency(jp.getDoubleValue());
+                    break;
+                case "numSamples":
+                    dbSnp.setMafSampleSize(jp.nextIntValue(0));
+                    break;
+                case "refAllele":
+                case "reference":
+                    String refAllele = jp.nextTextValue();
+                    if( dbSnp.getRefAllele()!=null ) {
+                        if( !refAllele.equals(dbSnp.getRefAllele()) ) {
+                            throw new Exception("ref allele override");
+                        }
+                    } else {
+                        dbSnp.setRefAllele(refAllele);
+                    }
+                    break;
+                case "type":
+                case "variantType":
+                    String snpType = jp.nextTextValue();
+                    if( dbSnp.getSnpClass()!=null ) {
+                        if( !snpType.equals(dbSnp.getSnpClass()) ) {
+                            throw new Exception("snp class override");
+                        }
+                    } else {
+                        dbSnp.setSnpClass(snpType);
+                    }
+                    break;
+                case "format":
+                    String format = jp.nextTextValue();
+                    if( !format.equals("GT") ) {
+                        throw new Exception("unexpected format: "+format);
+                    }
+                    break;
+                case "GT":
+                    String genotype = jp.nextTextValue();
+                    if( dbSnp.getGenotype()!=null ) {
+                        if( !genotype.equals(dbSnp.getGenotype()) ) {
+                            throw new Exception("genotype override");
+                        }
+                    } else {
+                        dbSnp.setGenotype(genotype);
+                    }
+                    break;
+                case "dbsnp-build":
+                    dbSnp.setSource("dbSnp"+jp.nextTextValue());
+                    break;
+                case "chromosome":
+                    dbSnp.setChromosome(jp.nextTextValue());
+                    break;
+                case "start":
+                    dbSnp.setPosition(jp.nextIntValue(0));
+                    break;
+                case "length":
+                    int snpLen = jp.nextIntValue(0);
+                    if( snpLen!=1 ) {
+                        throw new Exception("unexpected snp len: "+snpLen);
+                    }
+                    break;
+                case "ids":
+                    if( jp.nextToken() != JsonToken.START_ARRAY ) {
+                        throw new Exception("unexpected: no array for ids");
+                    }
+                    while( jp.nextToken()!= JsonToken.END_ARRAY ) {
+                        String id = jp.getText();
+                        if( id.startsWith("rs") ) {
+                            dbSnp.setSnpName(id);
+                        }
+                    }
+                    break;
+                case "genomic":
+                    if( jp.nextToken() != JsonToken.START_ARRAY ) {
+                        throw new Exception("unexpected: no array for genomic");
+                    }
+                    while( jp.nextToken()!= JsonToken.END_ARRAY ) {
+                        String id = jp.getText();
+                        if( id.contains(":g.") ) {
+                            if( dbSnp.getHgvs()==null ) {
+                                dbSnp.setHgvs(new ArrayList<>());
+                            }
+                            dbSnp.getHgvs().add(id);
+                        }
+                    }
+                    break;
+
+                case "altAlleleCount": // ignore this for a moment
+                case "altAlleleFreq": // ignore this for a moment
+                case "mgf": // ignore this for a moment
+                case "missingAlleles": // ignore this for a moment
+                case "missingGenotypes": // ignore this for a moment
+                case "refAlleleCount": // ignore this for a moment
+                case "refAlleleFreq": // ignore this for a moment
+                case "transition": // ignore this for a moment
+                case "transversion": // ignore this for a moment
+
+                case "attributes":
+                case "cohortStats":
+                case "end":
+                case "fileId":
+                case "hgvs":
+                case "samplesData":
+                case "sourceEntries":
+                case "studyId":
+                case "variants": // toplevel array of all db_snp variants
+                    break;
+
+                case "2010-1_SHR_2010-1_SHR":
+                case "ENSEMBL_CELERA":
+                case "ENSEMBL_CELERA_maf":
+                case "ENSEMBL_SHR_RAT":
+                case "ENSEMBL_SHR_RAT_maf":
+                case "RAT_COMPUTATIONAL_CELERA_RAT_COMPUTATIONAL_CELERA":
+                case "SD":
+                case "SHR/OLAIPCV":
+                    break;
+                default:
+                    System.out.println("unknown field "+fieldName);
+            }
+
         }
 
         jp.close();
+        br.close();
+
+        System.exit(-1);
+    }
+
+    void save(DbSnp dbSnp) {
+        System.out.println("save db_snp\n");
     }
 
     public String getUrl() {
